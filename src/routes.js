df06d753e0fc47fd091ef365a87bc09121fd360e
M. Syofi Azmi <muhammadsyofiazmi@gmail.com>
2023-03-25 17:45:51 +0800

    add delete note, complete

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 const {
-  addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler,
+  addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler,
 } = require('./handler');
 
 const routes = [
@@ -22,6 +22,11 @@ const routes = [
     method: 'PUT',
     path: '/notes/{id}',
     handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
   },
 ];
 
